f1453d7f777d84e1d1d6903f37d75af2b99b67b1
vishwajeetlara <35160304+vishwajeetlara@users.noreply.github.com>
2019-03-07 15:45:04 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApp.Models
+{
+    public class Employee
+    {
+        public int EmployeeId
+        {
+            get; set;
+        }
+        public string Name
+        {
+            get;set;
+        }
+        public string Gendar
+        {
+            set;get;
+        }
+        public string City
+        {
+            get;set;
+        }
+    }
+}
