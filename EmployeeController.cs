d1e7a2fae62fb673bce42a13f013a7f77b827d2d
vishwajeetlara <35160304+vishwajeetlara@users.noreply.github.com>
2019-03-07 15:43:49 +0530

    Add files via upload

---
+++ EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApp.Models;
+
+namespace MvcApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Details()
+        {
+            Employee employee = new Employee()
+            {
+                EmployeeId = 101,
+                Name = "Jon",
+                Gendar = "Male",
+                City="Banglore"
+            };
+            return View(employee);
+        }
+    }
+}
